a97bd5a186655848925f715c3ff2facff2dfb328
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 10:53:08 +0300

    PotentialGuest - created pojo

---
+++ src/main/java/com/hotel/usagetool/domain/PotentialGuest.java
@@ -0,0 +1,19 @@
+package com.hotel.usagetool.domain;
+
+import lombok.Data;
+
+/**
+ * Represents the potential guest <br>
+ * for use in estimating the hotel usage.
+ */
+@Data
+public class PotentialGuest {
+
+    private double price;
+    private Type type;
+    private boolean availableForReservation;
+
+    public enum Type {
+        PREMIUM, ECONOMY
+    }
+}
