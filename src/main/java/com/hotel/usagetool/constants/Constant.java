477059b24a8545908ce31843b74499562f3acb71
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 12:05:40 +0300

    Constant - create constant class for reusing properties

---
+++ src/main/java/com/hotel/usagetool/constants/Constant.java
@@ -0,0 +1,17 @@
+package com.hotel.usagetool.constants;
+
+/**
+ * Constant class to hold all constant values.
+ */
+public class Constant {
+
+    /**
+     * The premium price limit.
+     */
+    public static final int PREMIUM_PRICE_LIMIT = 100;
+
+    /**
+     * private constructor to eliminate instantiation.
+     */
+    private Constant(){}
+}
