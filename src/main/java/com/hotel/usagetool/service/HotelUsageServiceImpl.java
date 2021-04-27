529d6ebfeae2d2d583ff0f7ee29df98d0ad7300c
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 16:11:38 +0300

    HotelUsageServiceImpl - added javadocs

--- src/main/java/com/hotel/usagetool/service/HotelUsageServiceImpl.java
+++ src/main/java/com/hotel/usagetool/service/HotelUsageServiceImpl.java
@@ -19,47 +19,69 @@ import static com.hotel.usagetool.domain.PotentialGuest.Type.PREMIUM;
 public class HotelUsageServiceImpl implements HotelUsageService {
 
     @Override
-    public HotelUsage calculateHotelUsage(int freePremiumRooms, int freeEconomyRooms, List<Number> potentialGuestPrices) {
+    public HotelUsage calculateHotelUsage(int freePremiumRooms, int freeEconomyRooms,
+                                          List<Number> potentialGuestPrices) {
         List<PotentialGuest> potentialGuests = convertPricesToPotentialGuests(potentialGuestPrices);
 
         RoomsUsage premium = RoomsUsage.builder().rooms(freePremiumRooms).build();
         RoomsUsage economy = RoomsUsage.builder().rooms(freeEconomyRooms).build();
 
-        return calculateUsage(premium, economy, potentialGuests);
+        return calculateHotelTotalUsage(premium, economy, potentialGuests);
     }
 
-    private HotelUsage calculateUsage(RoomsUsage premium, RoomsUsage economy, List<PotentialGuest> potentialGuests) {
+    /**
+     * Calculate the hotel total usage in both premium  economy rooms , by a list of potentialGuests.
+     *
+     * @param premium the given premium usage.
+     * @param economy the given economy usage.
+     * @param potentialGuests the given potential guests list.
+     * @return the calculated hotel total usage.
+     */
+    private HotelUsage calculateHotelTotalUsage(RoomsUsage premium, RoomsUsage economy,
+                                                List<PotentialGuest> potentialGuests) {
         for (PotentialGuest guest : potentialGuests) {
 
             if (allRoomsReserved(premium, economy))
                 break;
 
             if (isReservationPossible(guest, PotentialGuest.Type.PREMIUM, premium)) {
-                updateReservationData(premium, guest, PotentialGuest.Type.PREMIUM);
+                reserve(premium, guest, PotentialGuest.Type.PREMIUM);
             }
 
             if (isReservationPossible(guest, PotentialGuest.Type.ECONOMY, premium)) {
-                updateReservationData(premium, guest, PotentialGuest.Type.UPGRADED);
+                reserve(premium, guest, PotentialGuest.Type.UPGRADED);
             }
 
             if (isReservationPossible(guest, PotentialGuest.Type.ECONOMY, economy)) {
-                updateReservationData(economy, guest, PotentialGuest.Type.ECONOMY);
+                reserve(economy, guest, PotentialGuest.Type.ECONOMY);
             }
         }
 
         return HotelUsage.builder().premium(premium).economy(economy).build();
     }
 
+    /**
+     * Convert the list of prices to a list of Potential guests , for ease of manipulation <br>
+     * in reservations and hotel usage calculation.
+     * @param potentialGuestPrices the given list of potential guests prices.
+     * @return the list of potential guests.
+     */
     private List<PotentialGuest> convertPricesToPotentialGuests(List<Number> potentialGuestPrices) {
         return potentialGuestPrices.stream()
                 .mapToDouble(Number::doubleValue)
                 .boxed()
                 .sorted((price1, price2) -> Double.compare(price2, price1))
-                .map(this::buildGuest)
+                .map(this::buildPotentialGuest)
                 .collect(Collectors.toList());
     }
 
-    private PotentialGuest buildGuest(Double price) {
+    /**
+     * Hide a Potential Guest builder inside a method , and create the new instance by a given price.
+     *
+     * @param price the given price.
+     * @return the new instance of PotentialGuest.
+     */
+    private PotentialGuest buildPotentialGuest(Double price) {
         return PotentialGuest.builder()
                 .price(price)
                 .type(selectCustomerType(price))
@@ -67,38 +89,102 @@ public class HotelUsageServiceImpl implements HotelUsageService {
                 .build();
     }
 
+    /**
+     * Returns a Potential Guest type based on a given price rule.
+     *
+     * @param price the given price.
+     * @return the PotentialGuest.Type .
+     */
     private PotentialGuest.Type selectCustomerType(Double price) {
         return price >= PREMIUM_PRICE_LIMIT ? PREMIUM : ECONOMY;
     }
 
-    private boolean isGuestOfType(PotentialGuest guest, PotentialGuest.Type guestType) {
-        return guest.getType().equals(guestType);
+    /**
+     * Returns a boolean result of the PotentialGuest type based <br>
+     * on the given potential guest <br>
+     * and a given type matching.
+     *
+     * @param potentialGuest the given guest.
+     * @param guestType      the given guest type.
+     * @return the boolean result.
+     */
+    private boolean isPotentialGuestOfType(PotentialGuest potentialGuest, PotentialGuest.Type guestType) {
+        return potentialGuest.getType().equals(guestType);
     }
 
+    /**
+     * Returns a boolean result based on comparison of reserved rooms to rooms <br>
+     * of a given rooms usage.
+     *
+     * @param roomsUsage the given rooms usage.
+     * @return the boolean result.
+     */
     private boolean isAnyRoomAvailable(RoomsUsage roomsUsage) {
         return roomsUsage.getReservedRooms() < roomsUsage.getRooms();
     }
 
-    private boolean isReservationPossible(PotentialGuest guest, PotentialGuest.Type guestType, RoomsUsage roomsUsage) {
-        return isGuestOfType(guest, guestType) && isAnyRoomAvailable(roomsUsage) && guest.isAvailableForReservation();
+
+    /**
+     * Returns a boolean result if a reservation is possible , on the comparison of <br>
+     * potential guest type , room availability and guest availability for reservation.
+     *
+     * @param potentialGuest the given potential guest.
+     * @param guestType      the given guest type.
+     * @param roomsUsage     the given room usage.
+     * @return the boolean result.
+     */
+    private boolean isReservationPossible(PotentialGuest potentialGuest,
+                                          PotentialGuest.Type guestType, RoomsUsage roomsUsage) {
+        return isPotentialGuestOfType(potentialGuest, guestType) && isAnyRoomAvailable(roomsUsage)
+                && potentialGuest.isAvailableForReservation();
     }
 
+    /**
+     * REturns a boolean result if all premium & economy rooms are reserved, <br>
+     * based on a given premium room usage and an economy room usage.
+     *
+     * @param premium the given premium usage.
+     * @param economy the given economy usage.
+     * @return the bollean result.
+     */
     private boolean allRoomsReserved(RoomsUsage premium, RoomsUsage economy) {
         return premium.getReservedRooms() == premium.getRooms() &&
                 economy.getReservedRooms() == economy.getRooms();
     }
 
-    private void updateReservationData(RoomsUsage roomsUsage, PotentialGuest potentialGuest, PotentialGuest.Type guestType) {
-        updateRoomsUsageData(roomsUsage, potentialGuest);
-        updatePotentialGuestData(potentialGuest, guestType);
+    /**
+     * Reserve a room , and delegate the reservation to both room & potential guest <br>
+     * that are bound for the reservation.
+     *
+     * @param roomsUsage     the room usage.
+     * @param potentialGuest the potential guest.
+     * @param guestType      the guest type of the reservation.
+     */
+    private void reserve(RoomsUsage roomsUsage, PotentialGuest potentialGuest, PotentialGuest.Type guestType) {
+        roomUsageReservation(roomsUsage, potentialGuest.getPrice());
+        potentialGuestReservation(potentialGuest, guestType);
     }
 
-    private void updateRoomsUsageData(RoomsUsage roomsUsage, PotentialGuest potentialGuest) {
-        roomsUsage.increaseUsageBy(potentialGuest.getPrice());
+    /**
+     * Mimics the reservation of a room and the changes in the room usage <br>
+     * which should take place, such as the price and the increment of the reserved rooms.
+     *
+     * @param roomsUsage the given rooms usage.
+     * @param price      the given price for the reservation.
+     */
+    private void roomUsageReservation(RoomsUsage roomsUsage, Double price) {
+        roomsUsage.increaseUsageBy(price);
         roomsUsage.incrementReservedRoomsBy(1);
     }
 
-    private void updatePotentialGuestData(PotentialGuest potentialGuest, PotentialGuest.Type guestType) {
+    /**
+     * Mimics the reservation of a potential guest and the changes in his data <br
+     * which should take place, such as guest type and the availableForReservation flag.
+     *
+     * @param potentialGuest the potential guest.
+     * @param guestType      the guest type with which the guest will make a reservation.
+     */
+    private void potentialGuestReservation(PotentialGuest potentialGuest, PotentialGuest.Type guestType) {
         potentialGuest.setType(guestType);
         potentialGuest.setAvailableForReservation(false);
     }
