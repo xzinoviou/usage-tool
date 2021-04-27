d993c6b4518f8498a4e72fc13e78bd67fbeb380f
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 10:29:18 +0300

    HotelUsageController - creation of the controller for the API

---
+++ src/main/java/com/hotel/usagetool/controller/HotelUsageController.java
@@ -0,0 +1,14 @@
+package com.hotel.usagetool.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Hotel usage API .<br>
+ * Exposes the operation for calculating a hotel usage.
+ */
+@RestController
+@RequestMapping("/hotel-usage")
+public class HotelUsageController {
+    
+}
