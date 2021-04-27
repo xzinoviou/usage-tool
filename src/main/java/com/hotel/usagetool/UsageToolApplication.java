bd94f28301887048d0c01dc4a373e0cee0dc55d6
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 09:03:41 +0300

    initial commit of spring boot project

---
+++ src/main/java/com/hotel/usagetool/UsageToolApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.usagetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsageToolApplication.class, args);
+    }
+
+}
