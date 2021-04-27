597644c15e45bddfa4e9b777732d676f413b6f5a
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-04-27 10:11:23 +0300

    added HotelUsageRequest to be used as the request payload

---
+++ src/main/java/com/hotel/usagetool/domain/HotelUsageRequest.java
@@ -0,0 +1,18 @@
+package com.hotel.usagetool.domain;
+
+import lombok.Getter;
+
+/**
+ * Represents the request sent from the hotel,<br>
+ * for the usage calculation , by given free rooms <br>
+ * per category(premium, economy) and a given array <br>
+ * of prices potential customers are willing to pay.
+ */
+@Getter
+public class HotelUsageRequest {
+
+    int freePremiumRooms;
+    int freeEconomyRooms;
+    Number[] potentialGuestPrices;
+
+}
